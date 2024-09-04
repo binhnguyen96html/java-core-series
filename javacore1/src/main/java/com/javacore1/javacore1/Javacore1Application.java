2f800c62c7ae4a530adee13aa702dbaddc7bb52e
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-09-03 22:12:34 -0700

    boxing and unboxing

--- javacore1/src/main/java/com/javacore1/javacore1/Javacore1Application.java
+++ javacore1/src/main/java/com/javacore1/javacore1/Javacore1Application.java
@@ -3,19 +3,30 @@ package com.javacore1.javacore1;
 // import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.javacore1.test.TestMain;
-
 @SpringBootApplication
 public class Javacore1Application {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(Javacore1Application.class, args);
-		
-		// CaChuon caChuon = new CaChuon();
-		// caChuon.boi();
 
-		TestMain testMain = new TestMain();
-		testMain.testAbc();
+		// Boxing is the process of converting a primitive type into its corresponding
+		// wrapper class object. This can be done manually or automatically
+		// (auto-boxing).
+		// primitive -> wrapper : boxing -> manually
+		int a = 50;
+		Integer a2 = new Integer(a);
+
+		// auto boxing
+		Integer a3 = 5;
+		
+
+		// Unboxing is the reverse process where the object of a wrapper class is
+		// converted back to its corresponding primitive type.
+		// manual unboxing
+		Integer b = new Integer(50);
+
+		// auto unboxing
+		int b2 = b;
 	}
 
 }
