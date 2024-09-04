13bffbb3950987ba55107aa5e97daeb13102a02b
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-09-03 20:58:38 -0700

    access modify: default, need to import folder/file to use

--- javacore1/src/main/java/com/javacore1/javacore1/Javacore1Application.java
+++ javacore1/src/main/java/com/javacore1/javacore1/Javacore1Application.java
@@ -3,15 +3,19 @@ package com.javacore1.javacore1;
 // import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.javacore1.test.TestMain;
+
 @SpringBootApplication
 public class Javacore1Application {
 
 	public static void main(String[] args) {
 		// SpringApplication.run(Javacore1Application.class, args);
 		
-		CaChuon caChuon = new CaChuon();
+		// CaChuon caChuon = new CaChuon();
+		// caChuon.boi();
 
-		caChuon.boi();
+		TestMain testMain = new TestMain();
+		testMain.testAbc();
 	}
 
 }
---
+++ javacore1/src/main/java/com/javacore1/test/TestMain.java
@@ -0,0 +1,7 @@
+package com.javacore1.test;
+
+public class TestMain {
+  public void testAbc(){
+    System.out.println("Print from testAbc method of test package");
+  }
+}
