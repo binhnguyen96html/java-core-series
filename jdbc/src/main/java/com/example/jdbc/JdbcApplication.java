858ad83d1aaceb96fb698beecdb34223ac512b83
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-09-04 23:44:42 -0700

    saved

--- jdbc/src/main/java/com/example/jdbc/JdbcApplication.java
+++ jdbc/src/main/java/com/example/jdbc/JdbcApplication.java
@@ -9,7 +9,6 @@ import java.sql.Statement;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class JdbcApplication {
 
@@ -21,37 +20,21 @@ public class JdbcApplication {
 	public static void main(String[] args) {
 		// SpringApplication.run(JdbcApplication.class, args);
 
-		System.out.println("hello");
-
 		String name = null;
 		String street = null;
 		String ward = null;
 		Integer floorArea = 100;
 		Integer numberOfBasement = 100;
 
-
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-
-		try {
-			// Step 1: Load driver 
-			// Old (deprecated)
-			// Class.forName("com.mysql.jdbc.Driver");
-
-			// Step 2: Create connection
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-
-			// Step 3: initialize statmenet (Khoi tao doi tuong)
-			stmt = conn.createStatement();
-
-			// Step 4: Execute sql (thuc thi cau sql)
-			rs = stmt.executeQuery(QUERY.toString());
-
+		//Experienced developer
+		try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(QUERY.toString());) {
+					
 			// metadata
 			ResultSetMetaData rsmd = rs.getMetaData();
 
-			while(rs.next()){
+			while (rs.next()) {
 				System.out.println("id: " + rs.getLong("id") + " , Type: " + rsmd.getColumnType(1));
 				System.out.println("name: " + rs.getString("name"));
 				System.out.println("street: " + rs.getString("street"));
@@ -60,23 +43,14 @@ public class JdbcApplication {
 				System.out.println("floorarea: " + rs.getInt("floorarea"));
 			}
 
-
 		} catch (SQLException e) {
 			System.out.println("Error: " + e.getMessage());
-		}catch(Exception e){
+			e.printStackTrace();
+		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
-		}finally{
-			//Always run this after run all above
-			try {
-				if(conn != null) conn.close();
-				if(stmt != null) stmt.close();
-				if(rs != null) rs.close();
-			} catch (Exception e) {
-				System.out.println("Error: " + e.getMessage());
-			}
+			e.printStackTrace();
 		}
 
-		
 	}
 
 }
